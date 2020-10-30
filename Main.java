f52022aadf90bdce068e65fa96f328d73694ae92
younj1601 <younj1601@wrdsb.ca>
2020-10-30 12:57:35 +0000

    Finished the c example

--- Main.java
+++ Main.java
@@ -5,7 +5,19 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+  JPanel topPanel;
+  JPanel buttonPanel;
+
+  JButton[] numberButtons;
+  JButton addButton;
+  JButton subtractButton;
+  JButton multButton;
+  JButton divButton;
+  JButton equalButton;
+  JButton decimalButton;
+
+  JTextField display;
 
 
   // Method to assemble our GUI
@@ -19,8 +31,86 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
-    
+    //initialize the main panel with a border layout
+    mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
 
+    //initialize the button panel with a grid layout
+    buttonPanel = new JPanel();
+    buttonPanel.setLayout(new GridLayout(4,4));
+
+    //initialize the top panel with the default layout
+    topPanel = new JPanel();
+
+    //create the display
+    display = new JTextField("0");
+    //set a perfered size so Flow layout knows it should be bigger
+    display.setPreferredSize(new Dimension(800,100));
+    //add it to the top
+    topPanel.add(display);
+
+    //creating the number buttons
+    numberButtons = new JButton[10];
+    for(int i = 0; i < numberButtons.length; i++){
+      numberButtons[i] = new JButton("" + i);
+      //set the action command
+      numberButtons[i].setActionCommand("" + i);
+      //add action Listener
+      numberButtons[i].addActionListener(this);
+    }
+
+    //create the operation buttons
+    equalButton = new JButton("=");
+    equalButton.addActionListener(this);
+    equalButton.setActionCommand("equal");
+
+    addButton = new JButton("+");
+    addButton.addActionListener(this);
+    addButton.setActionCommand("add");
+
+    subtractButton = new JButton("-");
+    subtractButton.addActionListener(this);
+    subtractButton.setActionCommand("subtract");
+
+    multButton = new JButton("x");
+    multButton.addActionListener(this);
+    multButton.setActionCommand("multiply");
+
+    divButton = new JButton("/");
+    divButton.addActionListener(this);
+    divButton.setActionCommand("divide");
+
+    decimalButton = new JButton(".");
+    decimalButton.addActionListener(this);
+    decimalButton.setActionCommand("decimal");
+
+    //add the buttons to the grid
+    buttonPanel.add(numberButtons[7]);
+    buttonPanel.add(numberButtons[8]);
+    buttonPanel.add(numberButtons[9]);
+    buttonPanel.add(divButton);
+
+    buttonPanel.add(numberButtons[4]);
+    buttonPanel.add(numberButtons[5]);
+    buttonPanel.add(numberButtons[6]);
+    buttonPanel.add(multButton);
+
+    buttonPanel.add(numberButtons[1]);
+    buttonPanel.add(numberButtons[2]);
+    buttonPanel.add(numberButtons[3]);
+    buttonPanel.add(subtractButton);
+
+    buttonPanel.add(numberButtons[0]);
+    buttonPanel.add(decimalButton);
+    buttonPanel.add(addButton);
+    buttonPanel.add(equalButton);
+
+    //assemble the panels
+    mainPanel.add(topPanel, BorderLayout.PAGE_START);
+    mainPanel.add(buttonPanel, BorderLayout.CENTER);
+    
+    //add the mail panel to the frame
+    frame.add(mainPanel);
   }
 
   // method called when a button is pressed
